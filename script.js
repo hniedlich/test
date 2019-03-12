1686ce39caf29fd1abe833d9246c497654f72cfb
hniedlich <46594399+hniedlich@users.noreply.github.com>
2019-03-12 09:33:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,5 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   // Required. Must be unique.
   id: "cms",
   // Required
-  label: "CMS",
-  subCategories: [
-    {
-      // Required. Must be unique.
-      id: "wordpress",
-      // Required.
-      label: "WordPress",
-      // Optional. If specified, defines a unique icon for this item.
-      icon: "icon-wordpress",
-      // Required. Items matching any tag will appear in this subcategory.
-      tags: [
-        "wordpress"
-      ]
-    }
-  ]
+  label: "CMS"
 });
