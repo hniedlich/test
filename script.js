5eb771deb31bebc5df212945244acc1a111fc6fa
hniedlich <46594399+hniedlich@users.noreply.github.com>
2019-04-10 09:52:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,31 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   id: "featured",
   // Required
   label: "Featured",
-  tags: [
+  subCategories: [
+    {
+      // Required. Must be unique.
+      id: "go",
+      // Required.
+      label: "Go",
+      // Optional. If specified, defines a unique icon for this item.
+      icon: "icon-go-gopher",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
         "go",
         "golang"
       ]
+    },
+    {
+      // Required. Must be unique.
+      id: "jenkins",
+      // Required.
+      label: "Jenkins",
+      // Optional. If specified, defines a unique icon for this item.
+      icon: "icon-jenkins",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
+        "jenkins"
+      ]
+    }
+  ]
 });
