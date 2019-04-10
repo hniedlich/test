05d64889f2af42b82507ed35c83cc1cba4f49dbb
hniedlich <46594399+hniedlich@users.noreply.github.com>
2019-04-10 09:37:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-// Add a Featured category as the first category tab.
-window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
-  // Required. Must be unique.
-  id: "hubapps",
+// Add a Featured category section at the top of the catalog
+window.OPENSHIFT_CONSTANTS.CATALOG_CATEGORIES.unshift({
+  // Required.  Must be unique
+  id: "featured",
   // Required
-  label: "HubApps :)",
-    items: [
+  label: "Featured",
+  // Optional.  If specified, each item in the category will utilize this icon
+  // as a default
+  iconClassDefault: "fa fa-code",
+  items: [
     {
       // Required.  Must be unique
-      id: "cms",
+      id: "go",
       // Required
-      label: "CMS",
+      label: "Go",
       // Optional.  If specified, defines a unique icon for this item
-      // iconClass: "font-icon icon-go-gopher",
+      iconClass: "font-icon icon-go-gopher",
       // Optional.  If specified, enables matching other tag values to this
       // category item
       categoryAliases: [
-        "CMS"
+        "golang"
       ],
       // Optional.  If specified, will display below the item label
-      description: "Made by Hubert"
+      description: "An open source programming language developed at Google in " +
+      "2007 by Robert Griesemer, Rob Pike, and Ken Thompson."
+    },
+    {
+      // Required.  Must be unique
+      id: "jenkins",
+      // Required
+      label: "Jenkins",
+      // Optional.  If specified, defines a unique icon for this item
+      iconClass: "font-icon icon-jenkins",
+      // Optional.  If specified, will display below the item label
+      description: "An open source continuous integration tool written in Java."
     }
   ]
 });
