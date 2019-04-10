03976faa657f7f0d2b088beb99bf81276576d815
hniedlich <46594399+hniedlich@users.noreply.github.com>
2019-04-10 08:56:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Add a Featured category as the first category tab.
 window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   // Required. Must be unique.
-  id: "cms",
+  id: "hubapps",
   // Required
-  label: "CMS"
+  label: "HubApps :)"
+    items: [
+    {
+      // Required.  Must be unique
+      id: "cms",
+      // Required
+      label: "CMS",
+      // Optional.  If specified, defines a unique icon for this item
+      // iconClass: "font-icon icon-go-gopher",
+      // Optional.  If specified, enables matching other tag values to this
+      // category item
+      categoryAliases: [
+        "CMS"
+      ],
+      // Optional.  If specified, will display below the item label
+      description: "Made by Hubert"
+    },
 });
