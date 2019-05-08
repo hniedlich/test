71148b4e9be964ace64f267d04a5d82b39aa2bf9
hniedlich <46594399+hniedlich@users.noreply.github.com>
2019-05-08 13:41:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,18 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
     },
     {
       // Required. Must be unique.
+      id: "gallery",
+      // Required.
+      label: "Gallery",
+      // Optional. If specified, defines a unique icon for this item.
+      // icon: "icon-jenkins",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
+        "gallery"
+      ]
+    },
+    {
+      // Required. Must be unique.
       id: "survey",
       // Required.
       label: "Survey",
